baa36c4f120505de9fb41ffbebf4fee9ef9aa9b1
Trialerror-phase <nyangayiotuya@gmail.com>
2024-11-03 23:19:43 +0100

    Update Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,6 +10,9 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <ul className="navbar-list">
+      <li className="navbar-item">
+          <Link to="/">{t('ngo')}</Link>
+        </li>
         <li className="navbar-item">
           <Link to="/">{t('home')}</Link>
         </li>
